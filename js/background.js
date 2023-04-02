f24f4aa3a78aee606047b9bf678c84cc54192c00
Dongmin_Kim <66549954+dongmin30@users.noreply.github.com>
2023-04-03 01:44:17 +0900

    css 기초 적용사항 반영 및 로그아웃 기능 추가

--- js/background.js
+++ js/background.js
@@ -1,13 +1,10 @@
 const images = [
     "img_1.jpg",
     "img_2.jpg",
-    "img_3.jpg"
+    "img_3.jpg",
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src= `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundRepeat = 'no-repeat';
---
+++ js/login.js
@@ -0,0 +1,43 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const logoutButton = document.querySelector("#logout");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const TODOS = "todos"
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function logoutButtonClick() {
+    if(confirm('정말 로그아웃 하시겠습니까?')){
+        localStorage.removeItem(USERNAME_KEY);
+        localStorage.removeItem(TODOS);
+        window.location.reload();
+        alert("로그아웃 되었습니다.");
+    } else {
+        return false;
+    }
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+logoutButton.addEventListener("click", logoutButtonClick);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
